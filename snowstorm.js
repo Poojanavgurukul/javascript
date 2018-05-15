2dcfd17e1e9cbf01767661da588ae97c48da9923
Pooja Saini <you@example.com>
2018-05-15 11:50:38 +0530

    my js

---
+++ snowstorm.js
@@ -0,0 +1,3 @@
+snowStorm.snowColor = 'white';   // blue-ish snow!?
+snowStorm.flakesMaxActive = 96;    // show more snow on screen at once
+snowStorm.useTwinkleEffect = true; // let the snow flicker in and out of view
